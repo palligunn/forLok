fa765ffae52ea8b3d7ece1f21232cd2322014c1f
palligunn <palligunn21@gmail.com>
2017-11-23 12:07:59 +0000

    fékk hugmyndina að nota kóðan úr vasareikninum, notandinn fær 6 tölur til að velja, smella á þær ekki skrifa, kemur í veg fyrir að notandinn fari fyrir ofan 6.
     ætla að reyna að commita þetta á annað branch bara ef að þetta er að fara eitthvað til fjandans

--- Valmynd.py
+++ Valmynd.py
@@ -1,33 +1,43 @@
 import threading
 import random
 from random import randint
-import os.path
 from tkinter import *
 import tkinter.messagebox
-import os
+
 
 
 def teningur():#if int(gisk) == dice: value error
-    def rulla():
+    def rulla(numbers):
         tening = randint(1,7)
-        if gisk == tening:
+        global operator
+        operator = operator + str(numbers)
+        if numbers == tening:
             tkinter.messagebox.showinfo('Rétt!')
-        if gisk != tening:
+        elif numbers != tening:
             tkinter.messagebox.showinfo('Vitlaust')
 
     root = Tk()
 
     root.geometry('170x120')#gluggastærð breiddXhæð
 
-    Label(root,text="Veldu tölu").pack()
+    #Label(root,text="Veldu tölu").pack()
 
+    operator = ""
+    text_Input = StringVar()
 
+    btn1 = Button(root, padx=4, pady=4, bd=2, fg="black", font=('arial', 20, 'bold'), text='1',command=lambda: rulla(1)).grid(row=1, column=0)
+    btn2 = Button(root, padx=4, pady=4, bd=2, fg="black", font=('arial', 20, 'bold'), text='2',command=lambda: rulla(2)).grid(row=1, column=1)
+    btn3 = Button(root, padx=4, pady=4, bd=2, fg="black", font=('arial', 20, 'bold'), text='3',command=lambda: rulla(3)).grid(row=1, column=2)
+    btn4 = Button(root, padx=4, pady=4, bd=2, fg="black", font=('arial', 20, 'bold'), text='4',command=lambda: rulla(4)).grid(row=2, column=0)
+    btn5 = Button(root, padx=4, pady=4, bd=2, fg="black", font=('arial', 20, 'bold'), text='5',command=lambda: rulla(5)).grid(row=2, column=1)
+    btn6 = Button(root, padx=4, pady=4, bd=2, fg="black", font=('arial', 20, 'bold'), text='6',command=lambda: rulla(6)).grid(row=2, column=2)
+    '''
     g = StringVar() #lætur 'g' vera að int
     tala = Entry(root, textvariable=g).pack()#talan er sleginn inn hér og verður talan = g
     gisk = g.get()#talan sem er inn í g er sett inn í gisk
-    Label(root,text=tala).pack()
 
     submit = Button(root, text = "Kasta tening", command = rulla).pack()
+    '''
     root.mainloop()
 
 #def annarleikur():
@@ -36,7 +46,7 @@ def teningur():#if int(gisk) == dice: value error
 #Root = glugginn
 
 root = Tk()
-root.title('Leikjasalernið')
+root.title('Leikjasalurinn')
 menubar = Menu(root)
 root.geometry("500x500") #stærðin af glugganum
 
@@ -47,15 +57,16 @@ one.pack(fill=X)
 
 btn1 = Button(text="Teningakast", command=teningur)
 btn1.pack()
-btn2 = Button(text="Skyrim")
+btn2 = Button(text="leikur2")
 btn2.pack()
-btn3 = Button(text="Overwatch")
+btn3 = Button(text="leikur3")
 btn3.pack()
 
+'''
 helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label="Hjálp")
 helpmenu.add_command(label="Um...")
 menubar.add_cascade(label="Hjálp", menu=helpmenu)
-
+'''
 root.config(menu=menubar)
 root.mainloop()
