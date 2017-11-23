ef81def64b22459906758e04380109783184abf0
pallsv227 <pallig1@gmail.com>
2017-11-23 10:58:34 +0000

    Fyrsta commit -Páll

--- Valmynd.py
+++ Valmynd.py
@@ -1,12 +1,35 @@
 import threading
 import random
+from random import randrange
 import os.path
 from tkinter import *
 import os
 
 
-def donothing():
-    os.system("snek.py 1")
+def teningur():#if int(guess) == dice: value error
+    def rulla():
+        dice = randrange(1,7)
+        if int(guess) == dice:
+            tkMessageBox.showinfo("Well Done!","Correct!")
+        if int(guess) > 6:
+            tkMessageBox.showinfo("Error"," Invalid number: try again")
+        elif int(guess) <= 0:
+            tkMessageBox.showinfo("Error"," Invalid number: try again")
+        else:
+            tkMessageBox.showinfo("Incorrect","Incorrect: dice rolled {}.".format(diceRoll))
+
+    root = Tk()
+
+    Label(root,text="Enter your guess").pack()
+
+    g = StringVar()
+    inputGuess = Entry(root, textvariable=g).pack()
+    guess = g.get() #changed variable from v to g
+
+    submit = Button(root, text = "Roll Dice", command = rulla).pack()
+    root.mainloop()
+
+#def annarleikur():
 
 
 #Root = glugginn
@@ -14,14 +37,14 @@ def donothing():
 root = Tk()
 root.title('Leikjasalernið')
 menubar = Menu(root)
-#root.geometry("500x500")
+root.geometry("500x500") #stærðin af glugganum
 
 
 topFrame=Frame(root)
 one=Label(root,text="Velkominn í leikjasalinn")
 one.pack(fill=X)
 
-btn1 = Button(text="Battlefield 1", command=donothing)
+btn1 = Button(text="Teningakast", command=teningur)
 btn1.pack()
 btn2 = Button(text="Skyrim")
 btn2.pack()
